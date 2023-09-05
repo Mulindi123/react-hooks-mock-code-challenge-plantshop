e6c6a431b5052d52698a6d047bc9b0f3d6b492b5
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-09-05 22:02:40 +0300

    Implements sold out functionality

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({plant}) {
+  const [isInStock, setIsInStock] = useState(true)
+
+  function toggleIsInStock(){
+    setIsInStock(!isInStock)
+  }
+
   return (
     <li className="card">
       <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
       <p>Price: {plant.price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <button className={`primary ${isInStock? "In Stock": "Out of Stock"}`}
+              onClick={toggleIsInStock}
+      >
+         {isInStock ? "In Stock": "Out of Stock"}     
+      </button>
     </li>
   );
 }
